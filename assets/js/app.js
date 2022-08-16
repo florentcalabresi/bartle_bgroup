39993389ab3a35faa7df790e2106b2465b742225
Florent Calabresi <60184656+florentcalabresi@users.noreply.github.com>
2022-08-16 02:23:16 +0200

    Add animations + Hover

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,31 @@
+$.fn.isInViewport = function() {
+    var elementTop = $(this).offset()?.top;
+    var elementBottom = elementTop + $(this).outerHeight();
+  
+    var viewportTop = $(window).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+  
+    return elementBottom > viewportTop && elementTop < viewportBottom;
+};
+
 $(window).on('scroll', function() {
     if($(window).scrollTop() >= 100)
         $('header nav').addClass('gradient')
     else
-        $('header nav').removeClass('gradient')
+        $('header nav').removeClass('gradient') 
 });
-$(document).ready(function() {
-    $('body').animate({opacity: 1.0}, 1000);
+
+var sections = ["titles", "news", "about", "activities", "notes", "join", "contact"];
+sections.forEach((el) => {
+    setInterval(() => {
+        if ($("section.categ#"+el).isInViewport())
+            if($('section.categ#'+el).css('opacity') == 0) 
+                $("section.categ#"+el).animate({opacity: 1}, 1000)
+    })
 })
 
-var Scrollbar = window.Scrollbar;
-Scrollbar.init(document.querySelector('#my-scrollbar'));
+$(document).ready(function() {
+    $('body').animate({opacity: 1.0}, 1000);
+    if($(window).scrollTop() >= 100)
+        $('header nav').addClass('gradient')
+})
